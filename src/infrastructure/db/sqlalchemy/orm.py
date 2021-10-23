8563c71af421e2cef21d3fcc262f6be8d74a3f25
Jarosław Orzeł <jaroslaw.orzel@emplocity.pl>
2021-10-23 18:47:28 +0200

    json column type JSONB for PG and JSON for sqlite

--- src/infrastructure/db/sqlalchemy/orm.py
+++ src/infrastructure/db/sqlalchemy/orm.py
@@ -1,12 +1,16 @@
 from datetime import datetime
+from typing import Any, Dict
 
-from sqlalchemy import Column, DateTime, Float, Integer, String, Table
+from sqlalchemy import Column, DateTime, Float, Integer, String, Table, Text
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.dialects.postgresql.pypostgresql import PGDialect_pypostgresql
 from sqlalchemy.dialects.sqlite import JSON
+from sqlalchemy.dialects.sqlite.pysqlite import SQLiteDialect_pysqlite
 from sqlalchemy.orm import mapper
 
 from domain.projections.candidate import CandidateProjection
 from domain.value_objects import AggregateId
-from infrastructure.db.sqlalchemy.setup import metadata
+from infrastructure.db.sqlalchemy.setup import engine, metadata
 
 
 class StoredEvent:
@@ -15,12 +19,29 @@ class StoredEvent:
     """
 
     def __init__(
-        self, originator_id: AggregateId, name: str, data: str, timestamp: datetime
+        self,
+        originator_id: AggregateId,
+        name: str,
+        data: Dict[str, Any],
+        timestamp: datetime,
     ):
         self.originator_id = originator_id
         self.data = data
         self.timestamp = timestamp
         self.name = name
+
+
+dialects_json_type_mapper = {
+    SQLiteDialect_pysqlite: JSON,
+    PGDialect_pypostgresql: JSONB,
+}
+
+if isinstance(engine.dialect, SQLiteDialect_pysqlite):
+    json_column_type = JSON
+elif isinstance(engine.dialect, PGDialect_pypostgresql):
+    json_column_type = JSONB
+else:
+    json_column_type = Text
 
 
 events = Table(
@@ -29,7 +50,7 @@ events = Table(
     Column("id", Integer, primary_key=True, autoincrement=True),
     Column("originator_id", String),
     Column("name", String),
-    Column("data", JSON),
+    Column("data", json_column_type),
     Column("timestamp", DateTime),
 )
 
@@ -39,7 +60,7 @@ candidate_projections = Table(
     Column("candidate_id", String, primary_key=True),
     Column("score", Float),
     Column("status", String),
-    Column("profile", JSON),
+    Column("profile", json_column_type),
     Column("added_at", DateTime),
     Column("invited_at", DateTime),
     Column("moved_to_standby_at", DateTime),
